67c7119a48c31f170bb9d9faf86a552508ed0b5c
davidrichardson <Dave@david-richardsons-imac.local>
2018-03-02 22:50:11 +0000

    expose ids for submissions + submittable

--- src/main/java/uk/ac/ebi/submission/store/config/RepositoryRestAdapterConfiguration.java
+++ src/main/java/uk/ac/ebi/submission/store/config/RepositoryRestAdapterConfiguration.java
@@ -2,8 +2,11 @@ package uk.ac.ebi.submission.store.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import uk.ac.ebi.submission.mapping.ObjectMappingConfig;
+import uk.ac.ebi.submission.store.submission.Submission;
+import uk.ac.ebi.submission.store.submittable.Submittable;
 
 @Configuration
 class RepositoryRestAdapterConfiguration extends RepositoryRestConfigurerAdapter {
@@ -13,4 +16,11 @@ class RepositoryRestAdapterConfiguration extends RepositoryRestConfigurerAdapter
         ObjectMappingConfig.customizeObjectMapper(objectMapper);
     }
 
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(
+                Submission.class,
+                Submittable.class
+        );
+    }
 }
