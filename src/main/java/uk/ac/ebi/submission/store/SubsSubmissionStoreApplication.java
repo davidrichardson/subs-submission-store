5cc9c72429d1e12960e99e4b2a17c8f7ba6998ec
davidrichardson <Dave@david-richardsons-imac.local>
2018-03-02 21:33:47 +0000

    Add curie support

--- src/main/java/uk/ac/ebi/submission/store/SubsSubmissionStoreApplication.java
+++ src/main/java/uk/ac/ebi/submission/store/SubsSubmissionStoreApplication.java
@@ -2,11 +2,26 @@ package uk.ac.ebi.submission.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.webmvc.RepositoryLinksResource;
+import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.hateoas.hal.CurieProvider;
+import org.springframework.hateoas.hal.DefaultCurieProvider;
+import uk.ac.ebi.submission.mapping.ObjectMappingConfig;
+import uk.ac.ebi.submission.messaging.MessageConverterConfig;
+import uk.ac.ebi.submission.store.root.RepositoryLinksResourceProcessor;
 
-@SpringBootApplication(scanBasePackageClasses = {uk.ac.ebi.submission.mapping.ObjectMappingConfig.class,uk.ac.ebi.submission.messaging.MessageConverterConfig.class})
+@SpringBootApplication(scanBasePackageClasses = {
+        SubsSubmissionStoreApplication.class,
+        ObjectMappingConfig.class,
+        MessageConverterConfig.class
+})
 public class SubsSubmissionStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SubsSubmissionStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SubsSubmissionStoreApplication.class, args);
+    }
+
 }
