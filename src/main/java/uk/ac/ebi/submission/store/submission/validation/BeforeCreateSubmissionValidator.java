aef018785fb50c45f1089afa92f1014ddff540fe
davidrichardson <Dave@david-richardsons-imac.local>
2018-03-03 20:28:50 +0000

    create WIP submission validator

---
+++ src/main/java/uk/ac/ebi/submission/store/submission/validation/BeforeCreateSubmissionValidator.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.submission.store.submission.validation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import uk.ac.ebi.submission.store.submission.Submission;
+
+@Component("beforeCreateSubmissionvalidator")
+@RequiredArgsConstructor
+@Slf4j
+public class BeforeCreateSubmissionValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Submission.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        log.info("{}",target);
+    }
+}
