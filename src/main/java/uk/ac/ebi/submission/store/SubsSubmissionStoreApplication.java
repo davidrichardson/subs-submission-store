242100fa1089b1c8d9f46d9dcd23f1f34bae5f16
davidrichardson <Dave@david-richardsons-imac.local>
2018-03-03 20:28:35 +0000

    remove unnecessary imports

--- src/main/java/uk/ac/ebi/submission/store/SubsSubmissionStoreApplication.java
+++ src/main/java/uk/ac/ebi/submission/store/SubsSubmissionStoreApplication.java
@@ -2,16 +2,8 @@ package uk.ac.ebi.submission.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.webmvc.RepositoryLinksResource;
-import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
-import org.springframework.hateoas.ResourceProcessor;
-import org.springframework.hateoas.UriTemplate;
-import org.springframework.hateoas.hal.CurieProvider;
-import org.springframework.hateoas.hal.DefaultCurieProvider;
 import uk.ac.ebi.submission.mapping.ObjectMappingConfig;
 import uk.ac.ebi.submission.messaging.MessageConverterConfig;
-import uk.ac.ebi.submission.store.root.RepositoryLinksResourceProcessor;
 
 @SpringBootApplication(scanBasePackageClasses = {
         SubsSubmissionStoreApplication.class,
