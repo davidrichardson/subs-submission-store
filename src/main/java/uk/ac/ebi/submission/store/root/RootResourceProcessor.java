1930a8429d1269bb6e0c0ffa906ffeb36085e3c6
davidrichardson <Dave@david-richardsons-imac.local>
2018-03-10 22:53:02 +0000

    simplify root endpoint

--- src/main/java/uk/ac/ebi/submission/store/root/RootResourceProcessor.java
+++ src/main/java/uk/ac/ebi/submission/store/root/RootResourceProcessor.java
@@ -1,24 +1,13 @@
 package uk.ac.ebi.submission.store.root;
 
-import uk.ac.ebi.submission.store.common.ResourceLinkHelper;
-import uk.ac.ebi.submission.store.submission.Submission;
-import uk.ac.ebi.submission.store.document.Document;
-import uk.ac.ebi.submission.store.documentType.DocumentType;
-import uk.ac.ebi.submission.store.validationResult.ValidationResult;
 import lombok.Data;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
-import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.Links;
 import org.springframework.hateoas.ResourceProcessor;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import uk.ac.ebi.submission.store.common.ResourceLinkHelper;
 
 @Component
 @Data
@@ -26,62 +15,19 @@ import java.util.List;
 public class RootResourceProcessor implements ResourceProcessor<RepositoryLinksResource> {
 
 
-    @NonNull
-    private RepositoryEntityLinks repositoryEntityLinks;
+    @Value("${aap.domains.url}")
+    private String aapApiRootUrl;
 
     @Override
     public RepositoryLinksResource process(RepositoryLinksResource resource) {
         log.debug("processing repository links resource {}", resource);
 
-        Class[] exposedClasses = new Class[]{
-                Submission.class,
-                Document.class,
-                DocumentType.class,
-                ValidationResult.class
-        };
-
-
-        for (Class clazz : exposedClasses) {
-            log.debug("adding links for {}",clazz);
-         //   resource.add(
-         //           templatedSingleResourceLink(clazz)
-         //   );
-
-        }
+        resource.add(new Link(aapApiRootUrl, "aapApiRoot"));
 
         log.debug("processed repository links resource {}", resource);
 
         ResourceLinkHelper.sortResourceLinksAlphabeticallyByRelName(resource);
 
         return resource;
-    }
-
-
-    private Link templatedSingleResourceLink(Class clazz) {
-        Link link = repositoryEntityLinks.linkToSingleResource(clazz, "***");
-        String templateText = link.getHref().replace("***", "{id}");
-        UriTemplate template = new UriTemplate(templateText);
-        return new Link(template, link.getRel());
-    }
-
-    private Collection<Link> searchResourceLink(Class clazz) {
-
-        Link collectionLink = repositoryEntityLinks.linkToCollectionResource(clazz).expand();
-
-        Links nativeSearchLinks = repositoryEntityLinks.linksToSearchResources(clazz);
-        List<Link> renamedLinks = new ArrayList<>();
-
-        for (Link searchLink : nativeSearchLinks) {
-            renamedLinks.add(
-                    new Link(
-                            searchLink.getTemplate(),
-                            collectionLink.getRel() + "-" + searchLink.getRel()
-                    )
-            );
-
-        }
-
-
-        return renamedLinks;
     }
 }
